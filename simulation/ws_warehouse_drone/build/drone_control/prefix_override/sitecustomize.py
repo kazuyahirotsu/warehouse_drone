de5d26671dc5a87d2eede25a8154ded6bc069032
Kazuya Hirotsu <hrtkzy4444@gmail.com>
2023-12-09 00:10:23 -0800

    deleted build folder

--- simulation/ws_warehouse_drone/build/drone_control/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/kazuya/warehouse_drone/simulation/ws_warehouse_drone/install/drone_control'
--- simulation/ws_warehouse_drone/build/px4_ros_com/colcon_command_prefix_build.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/kazuya/warehouse_drone/simulation/ws_warehouse_drone/install/px4_msgs/share/px4_msgs/package.sh"
